fc0efbf62fdd8f9b21280858ee4e1bbc4625b973
theresearchlab <aarontellis1996@outlook.com>
2023-10-08 18:34:51 -0400

    feat: add model file and call api data

--- Streamlit-Basketball-App/app.py
+++ Streamlit-Basketball-App/app.py
@@ -1,7 +1,16 @@
 import streamlit as st 
+import joblib 
+import requests
 
 st.snow()
 
+model = joblib.load('NBAHomeTeamWinLoss.pkl')
+
+url = "https://nba-prediction-api.onrender.com/all"
+response = requests.get(url)
+historic_games = pd.read_json(response.json(),orient='records')
+
+#upcoming_games =
 
 # Get Basketball Model
 # Show Accuracy and Precision

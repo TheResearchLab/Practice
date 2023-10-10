5f5fb206382c8ef69f054e06312aae48296e1df7
theresearchlab <aarontellis1996@outlook.com>
2023-10-09 20:26:16 -0400

    feat: update fct_wl_features table in jupyter notebook

--- Streamlit-Basketball-App/Data/update_stg_game_stats.py
+++ Streamlit-Basketball-App/Data/update_stg_game_stats.py
@@ -1,19 +1,9 @@
-from sqlalchemy import create_engine
 import pandas as pd
 import os
 import datetime
+import util_funcs
 
-from nba_api.stats.endpoints import cumestatsteamgames
-import time
-import requests
-
-
-database = os.getenv('DB_NAME')
-user = os.getenv('DB_USERNAME')
-host = os.getenv('DB_HOST')
-password = os.getenv('DB_PASSWORD')
-
-engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
+engine = util_funcs.engine
 
 game_stats_query = "SELECT * FROM stg_game_stats"
 game_stats = pd.read_sql_query(game_stats_query,engine)
@@ -31,7 +21,7 @@ nba_schedule_query = f"""SELECT  cast(gameId as signed) GAME_ID
                     stg_nba_schedule 
                 WHERE 
                     seriesText = ''
-                and gameDateTimeUTC < '{datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")}'
+                and gameDateTimeUTC > '{datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")}'
                 and postponedStatus = 'A'
                 and (homeTeamId <> 0 and awayTeamID <> 0)  """
 
@@ -39,13 +29,15 @@ nba_schedule = pd.read_sql_query(nba_schedule_query, engine)
 
 
 
-
+print(len(nba_schedule))
 
 
 for _,game in nba_schedule.iterrows():
     if game['GAME_ID'] not in game_stats['GAME_ID']:
-        getSingleGameMetrics(game['GAME_ID'],game['HOME_TEAM_ID'],game['AWAY_TEAM_ID'],game['AWAY_TEAM_NICKNAME'],game['SEASON'],game['GAME_DATE']).to_sql('stg_game_stats', engine, if_exists='append', index=False, method='multi')
+        util_funcs.getSingleGameMetrics(game['GAME_ID'],game['HOME_TEAM_ID'],game['AWAY_TEAM_ID'],game['AWAY_TEAM_NICKNAME'],game['SEASON'],game['GAME_DATE_UTC']).to_sql('stg_game_stats', engine, if_exists='append', index=False, method='multi')
     else:
         continue
+
+print('done')
 
 
--- Streamlit-Basketball-App/app.py
+++ Streamlit-Basketball-App/app.py
@@ -10,7 +10,15 @@ url = "https://nba-prediction-api.onrender.com/all"
 response = requests.get(url)
 historic_games = pd.read_json(response.json(),orient='records')
 
-#upcoming_games =
+# Get most recent features for away @ team
+historic_games['GAME_DATE'] = pd.to_datetime(historic_games['GAME_DATE'])
+away_max_dates = historic_games.groupby(['AWAY_TEAM_ID'])['GAME_DATE'].idxmax()
+away_team_df = historic_games.iloc[away_max_dates].reset_index().drop(['index'],axis=1) 
+
+# Get most recent features for team @ home
+home_max_dates = historic_games.groupby(['HOME_TEAM_ID'])['GAME_DATE'].idxmax()
+home_team_df = historic_games.iloc[home_max_dates].reset_index().drop(['index'],axis=1)
+home_team_df 
 
 # Get Basketball Model
 # Show Accuracy and Precision

f6f57a73b43a61c2ac70886b4c714696abf5acf7
theresearchlab <aarontellis1996@outlook.com>
2023-10-11 07:33:35 -0400

    feat: add mycursor mysql connection logic to util script

--- Streamlit-Basketball-App/Data/util_funcs.py
+++ Streamlit-Basketball-App/Data/util_funcs.py
@@ -5,6 +5,8 @@ import pandas as pd
 import json
 import os 
 from sqlalchemy import create_engine
+import mysql.connector as sql
+
 
 # Create DB Connection
 database = os.getenv('DB_NAME')
@@ -12,6 +14,13 @@ user = os.getenv('DB_USERNAME')
 host = os.getenv('DB_HOST')
 password = os.getenv('DB_PASSWORD')
 
+conn = sql.connect(host=host,
+                   database=database,
+                   user=user,
+                   password=password,
+                    )
+
+mycursor = conn.cursor()
 engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
 
 

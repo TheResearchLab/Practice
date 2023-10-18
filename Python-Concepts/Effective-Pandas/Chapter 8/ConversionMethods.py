f0963b19f36e44a8edfdff5bace541b510318a32
theresearchlab <aarontellis1996@outlook.com>
2023-10-18 06:53:55 -0400

    reading: finish chapter 8 section 2 with notes

--- Python-Concepts/Effective-Pandas/Chapter 8/ConversionMethods.py
+++ Python-Concepts/Effective-Pandas/Chapter 8/ConversionMethods.py
@@ -6,6 +6,7 @@ city_mpg = df['city08']
 highway_mpg = df['highway08']
 
 #Chapter 8.1
+print('===============Section 1================')
 
 # from dtype int64 to Int64
 print(city_mpg)
@@ -19,4 +20,13 @@ print(city_mpg.astype('Int16'))
 print(np.iinfo('int64')) #inspect limits on int & float types
 
 
+# Chapter 8.2
+print('===============Section 2================')
+print(city_mpg.nbytes)
+print(city_mpg.astype('int16').nbytes)
+
+make = df.make 
+print(make.nbytes) #only shows memory of pandas object (ancillary parts)
+print(make.memory_usage()) #this includes pandas object plus index memory
+print(make.memory_usage(deep=True)) # includes string objects themselves
 

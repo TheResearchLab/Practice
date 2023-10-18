420785e4b930f89dda0458eaa9192fbc3c795009
TheResearchLab <aarontellis1996@outlook.com>
2023-10-18 19:09:08 -0400

    reading: finish chapter 8.7 and take notes

--- Python-Concepts/Effective-Pandas/Chapter 8/ConversionMethods.py
+++ Python-Concepts/Effective-Pandas/Chapter 8/ConversionMethods.py
@@ -33,12 +33,31 @@ print(make.memory_usage(deep=True)) # includes string objects themselves
 
 
 # Chapter 8.3 
-print('===============Section 2================')
+print('===============Section 3================')
 print(city_mpg.astype('category'))
 print(city_mpg.astype(str))
 
-# order your own categorical data type
 
+# order your own categorical data type
+# Chapter 8.4
+print('===============Section 4================')
 sorted_values = pd.Series(sorted(set(city_mpg)))
 city_type = pd.CategoricalDtype(categories=sorted_values,ordered=True)
 print(city_mpg.astype(city_type))
+
+
+# Chapter 8.5
+print('===============Section 5================')
+print(city_mpg.to_frame())
+
+# Chapter 8.7
+print('===============Section 7================')
+print(city_mpg.astype('int16'))
+print(city_mpg.memory_usage(deep=True) - city_mpg.astype('int16').memory_usage(deep=True))
+
+str_convert = pd.Series([1,2,3,4,5,6,7,7,7,7,7,7,7],dtype=str)
+print(str_convert.astype('category'))
+print(str_convert.memory_usage(deep=True) -  str_convert.astype('category').memory_usage(deep=True))
+
+
+

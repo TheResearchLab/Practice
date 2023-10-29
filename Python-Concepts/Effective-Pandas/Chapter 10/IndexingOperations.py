327c48cd27ee0acec69ba74887ae129c8fa2e5ac
theresearchlab <aarontellis1996@outlook.com>
2023-10-29 11:32:11 -0400

    reading: finsish chapter 10.1 with notes

---
+++ Python-Concepts/Effective-Pandas/Chapter 10/IndexingOperations.py
@@ -0,0 +1,18 @@
+# Indexing Operations 
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/hadley/fueleconomy/master/data-raw/vehicles.csv'
+df = pd.read_csv(url)
+city_mpg = df['city08']
+highway_mpg = df['highway08']
+
+
+# Chapter 10.1 Data Prep
+city2 = city_mpg.rename(df['make'].to_dict())
+
+print(city2.index)
+
+#Passing scalar values
+city2 = city_mpg.rename(df['make'])
+print(city2)
+

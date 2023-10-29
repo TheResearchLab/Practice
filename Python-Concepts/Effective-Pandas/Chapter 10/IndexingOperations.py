882d291cb9af0bd860f25800b8fdfaf250cebd0b
theresearchlab <aarontellis1996@outlook.com>
2023-10-29 12:05:24 -0400

    reading: finish Chapter 10.2 with notes

--- Python-Concepts/Effective-Pandas/Chapter 10/IndexingOperations.py
+++ Python-Concepts/Effective-Pandas/Chapter 10/IndexingOperations.py
@@ -10,9 +10,14 @@ highway_mpg = df['highway08']
 # Chapter 10.1 Data Prep
 city2 = city_mpg.rename(df['make'].to_dict())
 
-print(city2.index)
+#print(city2.index)
 
 #Passing scalar values
 city2 = city_mpg.rename(df['make'])
-print(city2)
+#print(city2)
 
+# Chapter 10.2 Resetting the Index
+print(
+    city2.reset_index(),
+    city2.reset_index(drop=True) 
+)

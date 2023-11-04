6d4d9dfeaab437590a5d957b7d56e9efbb5a0e59
theresearchlab <aarontellis1996@outlook.com>
2023-11-04 06:49:09 -0400

    reading: finish chapter 13.5 with notes

--- Python-Concepts/Effective-Pandas/Chapter 13/datesInTheIndex.py
+++ Python-Concepts/Effective-Pandas/Chapter 13/datesInTheIndex.py
@@ -43,8 +43,14 @@ winter = (snow.index.quarter ==1) | (snow.index.quarter==4)
 
 
 # Chapter 13.4 Dropping Missing Values
+# print(
+#     snow
+#         .loc['1987-12-30':'1988-01-10']
+#         .dropna()
+# )
+
+# Chapter 13.5 Shifting data
 print(
-    snow
-        .loc['1987-12-30':'1988-01-10']
-        .dropna()
+    snow.shift(1),
+    snow.shift(-1)
 )

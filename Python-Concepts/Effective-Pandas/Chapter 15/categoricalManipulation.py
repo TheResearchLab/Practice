4462269c1b73347dda4209c16d5e54f128f32f35
theresearchlab <aarontellis1996@outlook.com>
2023-11-11 00:52:52 -0500

    reading: finish chapter 15.3 with notes

--- Python-Concepts/Effective-Pandas/Chapter 15/categoricalManipulation.py
+++ Python-Concepts/Effective-Pandas/Chapter 15/categoricalManipulation.py
@@ -1,4 +1,6 @@
 #%%
+
+
 import pandas as pd
 
 url = 'https://raw.githubusercontent.com/hadley/fueleconomy/master/data-raw/vehicles.csv'
@@ -7,11 +9,33 @@ df = pd.read_csv(url)
 make = df.make
 
 # Chapter 15.2 Frequency counts
-print(
-    make.value_counts(),
-    make.shape,
-    make.nunique()
+#print(
+#     make.value_counts(),
+#     make.shape,
+#    make.nunique()
+# )
 
-)
+# Chapter 15.3  Benefits of Categories
+
+cat_make = make.astype('category')
+# print(
+#     make.memory_usage(deep=True),
+#     cat_make.memory_usage(deep=True) #lower memory usage
+# )
+
+import timeit 
+code = """cat_make.str.upper()"""
+execution_time = timeit.timeit(stmt=lambda: eval(code),number=1)
+print(f'execution time: {execution_time}')
+
+code = """make.str.upper()""" #make is slower
+execution_time = timeit.timeit(stmt=lambda: eval(code),number=1)
+print(f'execution time: {execution_time}')
+
+
+
+
 
 # %%
+
+

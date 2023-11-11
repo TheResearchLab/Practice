4eb2ef304cb8a53a2ce35bf9f8ef715d4b87f476
theresearchlab <aarontellis1996@outlook.com>
2023-11-11 16:06:35 -0500

    reading: finish chapter 15.4 with notes

--- Python-Concepts/Effective-Pandas/Chapter 15/categoricalManipulation.py
+++ Python-Concepts/Effective-Pandas/Chapter 15/categoricalManipulation.py
@@ -23,16 +23,32 @@ cat_make = make.astype('category')
 #     cat_make.memory_usage(deep=True) #lower memory usage
 # )
 
-import timeit 
-code = """cat_make.str.upper()"""
-execution_time = timeit.timeit(stmt=lambda: eval(code),number=1)
-print(f'execution time: {execution_time}')
+# import timeit 
+# code = """cat_make.str.upper()"""
+# execution_time = timeit.timeit(stmt=lambda: eval(code),number=1)
+# print(f'execution time: {execution_time}')
 
-code = """make.str.upper()""" #make is slower
-execution_time = timeit.timeit(stmt=lambda: eval(code),number=1)
-print(f'execution time: {execution_time}')
+# code = """make.str.upper()""" #make is slower
+# execution_time = timeit.timeit(stmt=lambda: eval(code),number=1)
+# print(f'execution time: {execution_time}')
 
 
+# Chapter 15.4 Conversion to Ordinal Categories
+# Convert 'make' column to lowercase
+make = make.str.lower()
+
+# Create an ordered categorical type based on the sorted unique values
+make_type = pd.CategoricalDtype(categories=sorted(make.unique()), ordered=True)
+
+# Apply the categorical type to the 'make' column
+ordered_make = make.astype(make_type)
+
+print(
+    ordered_make,
+    ordered_make.max(),
+    #cat_make.max() typeerror
+    ordered_make.sort_values()
+)
 
 
 

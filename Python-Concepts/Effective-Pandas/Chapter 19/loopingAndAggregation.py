ad8ebca38846bd5915fce5a4821eaa5b2b0792d1
theresearchlab <aarontellis1996@outlook.com>
2023-11-19 10:51:11 -0500

    reading: finish Chapter 19.1 with notes

---
+++ Python-Concepts/Effective-Pandas/Chapter 19/loopingAndAggregation.py
@@ -0,0 +1,70 @@
+#%%
+import pandas as pd
+
+url = 'https://github.com/mattharrison/datasets/raw/master/data/siena2018-pres.csv'
+df = pd.read_csv(url, index_col=0)
+
+
+#df.select_dtypes('int64')
+def tweak_siena_pres(df):
+    def int64_to_uint8(df_):
+        cols = df_.select_dtypes('int64')
+        return df_.astype({col: 'uint8' for col in cols})
+
+    return (
+        df
+        .rename(columns={'Seq.': 'Seq'})  # 1
+        .rename(columns={k: v.replace(' ', '_') for k, v in
+                         {'Bg': 'Background',
+                          'PL': 'Party_leadership',
+                          'CAb': 'Communication_ability',
+                          'RC': 'Relations_with_Congress',
+                          'CAp': 'Court_appointments',
+                          'HE': 'Handling_of_economy',
+                          'L': 'Luck',
+                          'AC': 'Ability_to_compromise',
+                          'WR': 'Willing_to_take_risks',
+                          'EAp': 'Executive_appointments',
+                          'OA': 'Overall_ability',
+                          'Im': 'Imagination',
+                          'DA': 'Domestic_accomplishments',
+                          'Int': 'Integrity',
+                          'EAb': 'Executive_ability',
+                          'FPA': 'Foreign_policy_accomplishments',
+                          'LA': 'Leadership_ability',
+                          'IQ': 'Intelligence',
+                          'AM': 'Avoid_crucial_mistakes',
+                          'EV': "Experts_view",
+                          'O': 'Overall'}.items()})
+        .astype({'Party': 'category'})  # 2
+        .pipe(int64_to_uint8)  # 3
+        .assign(Average_rank=lambda df_: (df_.select_dtypes('uint8')  # 4
+                                          .sum(axis=1)
+                                          .rank(method='dense')
+                                          .astype('uint8')),
+                Quartile=lambda df_: pd.qcut(df_.Average_rank, 4,
+                                             labels='1st 2nd 3rd 4th'.split())
+                )
+    ) 
+
+pres = tweak_siena_pres(df)
+
+
+# Chapter 19.1 For Loops
+
+for key,value in pres.items():
+    # print(
+    #     f'label: {key}',
+    #     f'content:{value}'
+    #     )
+    break
+
+for idx,row in pres.iterrows():
+    #print(f'index:{idx}',type(row))
+    break
+
+for tup in pres.itertuples():
+    print(f'index:{tup[0]}',tup.Party)
+    break
+
+# %%

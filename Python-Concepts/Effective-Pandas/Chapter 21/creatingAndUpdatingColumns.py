86503a935f5d7ecd64149c2d9c18399077177fc0
TheResearchLab <aarontellis1996@outlook.com>
2023-11-25 21:35:29 -0500

    reading: start chapter 21.2 with notes

--- Python-Concepts/Effective-Pandas/Chapter 21/creatingAndUpdatingColumns.py
+++ Python-Concepts/Effective-Pandas/Chapter 21/creatingAndUpdatingColumns.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 url = 'https://github.com/mattharrison/datasets/raw/master/data/2020-jetbrains-python-survey.csv'
 df = pd.read_csv(url)
-df.columns
+
+df = df.loc[:,~df.columns.duplicated(keep='first')]
 
 
 import collections
@@ -42,13 +43,30 @@ for cols in counter.values():
 # )
 
 # lmao his bug example was fixed
-(df
-    [uniq_cols]
-    .rename(columns=lambda c:c.replace('.','_'))
-    ['age']
-    .iloc[:,1:]
-    ['age']
-    .str.slice(0,2)
-    .astype('Int64')
+# (df
+#     [uniq_cols]
+#     .rename(columns=lambda c:c.replace('.','_'))
+#     ['age']
+#     .iloc[:,1:]
+#     ['age']
+#     .str.slice(0,2)
+#     .astype('Int64')
+# )
+
+# 21.2 More Column Cleanup
+
+import numpy as np
+(
+    df
+    .rename(columns=lambda c: c.replace('.', '_'))
+    .assign(age=lambda df_: pd.to_numeric(df_['age'].astype(str).str.slice(0, 2),errors='coerce')
+                .astype('Int64'),
+            are_you_datascientist=lambda df_:df_['are_you_datascientist']
+                .replace({'Yes':True,'No':False,np.nan:False})
+        )
+        ['company_size']
+        .value_counts(dropna=False)                
 )
+
+
 # %%

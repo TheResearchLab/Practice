2ffbce6de590031436e27a98c98fb9027f3b89b5
TheResearchLab <aarontellis1996@outlook.com>
2023-12-06 08:32:46 -0500

    reading: finish Chapter 25.1 with notes

---
+++ Python-Concepts/Effective-Pandas/Chapter 25/plottingWithDataframes.py
@@ -0,0 +1,85 @@
+#%%
+import matplotlib.pyplot as plt
+import pandas as pd
+
+url = 'https://github.com/mattharrison/datasets/raw/master/data/siena2018-pres.csv'
+df = pd.read_csv(url, index_col=0)
+
+
+def tweak_siena_pres(df):
+    def int64_to_uint8(df_):
+        cols = df_.select_dtypes('int64')
+        return df_.astype({col: 'uint8' for col in cols})
+
+    return (
+        df
+        .rename(columns={'Seq.': 'Seq'})  # 1
+        .rename(columns={k: v.replace(' ', '_') for k, v in
+                         {'Bg': 'Background',
+                          'PL': 'Party_leadership',
+                          'CAb': 'Communication_ability',
+                          'RC': 'Relations_with_Congress',
+                          'CAp': 'Court_appointments',
+                          'HE': 'Handling_of_economy',
+                          'L': 'Luck',
+                          'AC': 'Ability_to_compromise',
+                          'WR': 'Willing_to_take_risks',
+                          'EAp': 'Executive_appointments',
+                          'OA': 'Overall_ability',
+                          'Im': 'Imagination',
+                          'DA': 'Domestic_accomplishments',
+                          'Int': 'Integrity',
+                          'EAb': 'Executive_ability',
+                          'FPA': 'Foreign_policy_accomplishments',
+                          'LA': 'Leadership_ability',
+                          'IQ': 'Intelligence',
+                          'AM': 'Avoid_crucial_mistakes',
+                          'EV': "Experts_view",
+                          'O': 'Overall'}.items()})
+        .astype({'Party': 'category'})  # 2
+        .pipe(int64_to_uint8)  # 3
+        .assign(Average_rank=lambda df_: (df_.select_dtypes('uint8')  # 4
+                                          .sum(axis=1)
+                                          .rank(method='dense')
+                                          .astype('uint8')),
+                Quartile=lambda df_: pd.qcut(df_.Average_rank, 4,
+                                             labels='1st 2nd 3rd 4th'.split())
+                )
+    ) 
+
+
+pres = tweak_siena_pres(df)
+
+# Chapter 25 line plots
+
+pres.plot().legend(bbox_to_anchor=(1,1))
+
+fig,ax=plt.subplots(dpi=600,figsize=(10,4))
+colors = []
+
+def set_colors(df):
+    for col in df.columns:
+        if 'George' in col:
+            colors.append('#990000')
+        else:
+            colors.append('#999999')
+    return df
+            
+
+(pres
+    .set_index('President')
+    .loc[::2,'Background':'Overall'] #gets every other president
+    .T
+    .pipe(set_colors)
+    .plot(ax=ax,rot=45,color=colors)
+    .legend(bbox_to_anchor=(1,1))
+)
+ax.set_xticks(range(21))
+ax.set_xticklabels(pres
+                    .loc[:,'Background':'Overall'].columns, ha='right')
+ax.set_ylabel('Rank')
+
+plt.show()
+
+
+# %%

6b3645ab3046364fa4c396716ebade309e1dbf24
TheResearchLab <aarontellis1996@outlook.com>
2023-12-11 07:20:11 -0500

    reading: finish chapter 26.3 with notes

--- Python-Concepts/Effective-Pandas/Chapter 26/reshapingDataframesWithDummies.py
+++ Python-Concepts/Effective-Pandas/Chapter 26/reshapingDataframesWithDummies.py
@@ -6,13 +6,13 @@ jb = pd.read_csv(url)
 
 # Chapter 26.1 Dummy Colums
 
-jb.filter(like='job.role') # get all columns like
+# jb.filter(like='job.role') # get all columns like
 
-(jb
-    .filter(like='job.role.*t') # collapse values into single column
-    .where(jb.isna(),1)
-    .fillna(0)    
-) 
+# (jb
+#     .filter(like='job.role.*t') # collapse values into single column
+#     .where(jb.isna(),1)
+#     .fillna(0)    
+# ) 
 
 job = (jb
     .filter(like='job.role') # collapse values into single column
@@ -25,6 +25,45 @@ job = (jb
 job
 
 dum = pd.get_dummies(job,dtype=int)
-dum
+# dum
+
+# Chapter 26.2 Undoing Dummy Columns
+
+import numpy as np
+dum.idxmax(axis='columns') # easiest read method
+
+i,j = np.where(dum)
+pd.Series(dum.columns[j],i) # this method is 8x faster than idxmax
+
+# Chapter 26.4 Exercises
+
+# Set seed for reproducibility
+np.random.seed(42)
+
+# Define categorical data
+categories = ['A', 'B', 'C', 'D']
+
+# Create a DataFrame with categorical columns and some null values
+data = {
+    'Category1': np.random.choice(categories + [np.nan], size=10),
+    'Category2': np.random.choice(categories + [np.nan], size=10),
+    'NumericColumn': np.random.randint(1, 100, size=10)
+}
+
+df = pd.DataFrame(data)
+df.replace('nan',np.nan,inplace=True)
+
+df1 = (df
+  .filter(like='Category')
+  #.replace('nan',np.nan)
+  .where(df.isna(),1)
+  .replace(np.nan,0)
+)
+
+df2 = df1.idxmax(axis='columns') # remove dummy columns
+
+pd.get_dummies(df2,dtype=int) # create dummy columns
+
+
 
 # %%

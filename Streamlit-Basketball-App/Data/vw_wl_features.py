7e00a24e191a8435bb65de8f86a8d18206733811
TheResearchLab <aarontellis1996@outlook.com>
2023-12-10 20:51:51 -0500

    practice: find first unique character in string

--- Streamlit-Basketball-App/Data/vw_wl_features.py
+++ Streamlit-Basketball-App/Data/vw_wl_features.py
@@ -7,9 +7,9 @@ query = """ select wf.away_team_id as team_id
                   ,wf.season
                   ,wf.away_last_game_oe as last_game_oe
                   ,wf.away_last_game_home_win_pctg as last_game_home_win_pctg
-                  ,wf.away_num_rest_days as num_rest_days
+                  ,wf.away_rest_day_cnt as num_rest_days
                   ,wf.away_last_game_away_win_pctg as last_game_away_win_pctg
-                  ,wf.away_last_game_total_win_pctg as last_game_total_win_pctg
+                  ,wf.away_last_game_season_win_pctg as last_game_total_win_pctg
                   ,wf.away_last_game_rolling_scoring_margin as last_game_rolling_scoring_margin
                   ,wf.away_last_game_rolling_oe as last_game_rolling_oe
             from wrk_wl_features wf
@@ -23,9 +23,9 @@ query = """ select wf.away_team_id as team_id
                   ,wf.season
                   ,wf.home_last_game_oe as last_game_oe
                   ,wf.home_last_game_home_win_pctg as last_game_home_win_pctg
-                  ,wf.home_num_rest_days as num_rest_days
+                  ,wf.home_rest_day_cnt as num_rest_days
                   ,wf.home_last_game_away_win_pctg as last_game_away_win_pctg
-                  ,wf.home_last_game_total_win_pctg as last_game_total_win_pctg
+                  ,wf.home_last_game_season_win_pctg as last_game_total_win_pctg
                   ,wf.home_last_game_rolling_scoring_margin as last_game_rolling_scoring_margin
                   ,wf.home_last_game_rolling_oe as last_game_rolling_oe
             from wrk_wl_features wf

8470e0adb838a8835b5e11319c909478887141be
TheResearchLab <aarontellis1996@outlook.com>
2024-02-14 19:31:59 -0500

    reading: CH-4 Normalizing Unicode for Reliable Comparisons

--- Python-Concepts/Fluent Python/UnicodeTextVsBytes.py
+++ Python-Concepts/Fluent Python/UnicodeTextVsBytes.py
@@ -14,4 +14,29 @@ cafe[:1]
 cafe_arr = bytearray(cafe)
 cafe_arr[-1:]
 
+#COPING WITH UnicodeDecodeError
+octets = b'Montr\xe9al'
+octets.decode('iso8859_7')
+#octets.decode('utf_8') # given utf_8 the variable contains unexpected byte characters
+octets.decode('utf_8',errors='replace')
+
+# BOM: A USEFUL GREMLIN
+u16 = 'El Niño'.encode('utf_16')
+u16 # this is denoted with b'\xff\xfeE' for little-endian
+
+list(u16) # 255 and 254 are the little-endian's encoding
+
+u16le = 'El Niño'.encode('utf_16le')
+list(u16le) # this is explicitly little-endina
+
+u16be = 'El Niño'.encode('utf_16be')
+list(u16be) # while le is 69 0 the be is 0 69
+
+# NORMALIZING UNICODE FOR RELIABLE COMPARISONS
+
+s1 = 'café'
+s2 = 'cafe\N{COMBINING ACUTE ACCENT}'
+s1,s2
+len(s1),len(s2)# different character lengths
+s1 == s2 #not equal
 # %%

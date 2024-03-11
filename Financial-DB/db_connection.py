4485d04cc1fd860efe19e8f99c84e6b29dbbf713
TheResearchLab <aarontellis1996@outlook.com>
2024-03-10 21:39:37 -0400

    practice: load tickers and exchanges to mysql

--- Financial-DB/db_connection.py
+++ Financial-DB/db_connection.py
@@ -1,13 +1,17 @@
 import os 
 from sqlalchemy import create_engine
 import mysql.connector as sql
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
 # Create DB Connection
 database = os.getenv('DB_NAME')
 user = os.getenv('DB_USERNAME')
 host = os.getenv('DB_HOST')
 password = os.getenv('DB_PASSWORD')
+
 
 conn = sql.connect(host=host,
                    database=database,
--- Financial-DB/fundamental.py
+++ Financial-DB/fundamental.py
@@ -1,21 +1,40 @@
-#%%
 
 import requests
 import pandas as pd
 from db_connection import engine
 
-filename = r"api_key.txt"
+filename = r"Financial-DB\api_key.txt"
 
 with open(filename,'r') as file:
     api_key = file.readline()
 
+def get_exchange_df() -> pd.DataFrame:
+    url = f'https://eodhd.com/api/exchanges-list/?api_token={api_key}&fmt=json'
+    exchange_list = requests.get(url).json()
+    return pd.DataFrame.from_dict(exchange_list)
 
-url = f'https://eodhd.com/api/exchanges-list/?api_token={api_key}&fmt=json'
-exchange_list = requests.get(url).json()
+def get_ticker_df(exchange_list: pd.Series) -> pd.DataFrame:
+    ticker_df = pd.DataFrame()
+    for exchange in exchange_list:
+        url = f'https://eodhd.com/api/exchange-symbol-list/{exchange}?api_token={api_key}&fmt=json'
+        ticker_list = requests.get(url).json()
+        ticker_df = pd.concat([ticker_df,pd.DataFrame.from_dict(ticker_list)])
+    return ticker_df.reset_index()
 
-url = f'https://eodhd.com/api/exchange-symbol-list/{"US"}?api_token={api_key}&fmt=json'
-ticker_list = requests.get(url).json()
 
-pd.DataFrame.from_dict(exchange_list).to_sql(name="stg_exchange",con=engine,if_exists="replace")
+
+# main function 
+chunksize = 1000
+exchange_df = get_exchange_df()
+ticker_df = get_ticker_df(exchange_df.Code)
+
+exchange_df.to_sql(name="financial_db.stg_exchange",con=engine,if_exists="replace")
+
+
+for i in range(0, len(ticker_df), chunksize):
+    chunk = ticker_df[i:i+chunksize]  # Get the chunk of data
+    chunk.to_sql("financial_db.stg_ticker", con=engine, if_exists='append', index=False)
+
+
 #pd.DataFrame.from_dict(ticker_list).to_sql(name="stg_")
-# %%
+

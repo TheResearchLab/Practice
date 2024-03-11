87f368d5c0a363b8f40c3e8bd57f7ce98c3e4d53
TheResearchLab <aarontellis1996@outlook.com>
2024-03-11 07:49:26 -0400

    reading: CH-8 Generic Mappings and Type Hinting

--- Python-Concepts/Fluent-Python/TypeHintInFunction.py
+++ Python-Concepts/Fluent-Python/TypeHintInFunction.py
@@ -1,14 +1,45 @@
 #%% 
-from typing import Optional
+from typing import Optional,Any
 
-def show_count(count: int, singular: str, plural: Optional[str] = None) -> str:
-    if count == 1:
-        return f'1 {singular}'
-    count_str = str(count) if count else 'no'
-    return f'{count_str} {plural}s'
+# def show_count(count: int, singular: str, plural: Optional[str] = None) -> str:
+#     if count == 1:
+#         return f'1 {singular}'
+#     count_str = str(count) if count else 'no'
+#     return f'{count_str} {plural}s'
 
-def test_irregular() -> None:
-    got = show_count(2,'child','children')
-    assert got == '2 children'
+# def test_irregular() -> None:
+#     got = show_count(2,'child','children')
+#     assert got == '2 children'
+
+# class Bird:
+#     pass
+
+# class Duck(Bird):
+#     def quack(self):
+#         print('Quack')
+
+# def alert(birdie):
+#     birdie.quack() # should be no problem here
+
+# def alert_duck(birdie: Duck) -> None:
+#     birdie.quack()
+
+# def alert_bird(birdie: Bird) -> None:
+#     birdie.quack() # this returns err in mypy because Bird type does not implement quack method
+
+# any vs object
+def double(x: Any) -> Any: # this works
+    return x * 2
+
+def double(x:object) -> object: # this breaks mypy because object doesn't support __mul__
+    return x * 2
+
+# GENERIC COLLECTIONS
+
+def tokenize(text: str) -> list[str]:
+    return text.upper().split()
+
+# GENERIC MAPPINGS
 
 
+# %%

2c30d150eee05c2768f1854278752cf6a4c92560
theresearchlab <aarontellis1996@outlook.com>
2024-03-10 20:18:10 -0400

    practice: convert json data to df and send to mysql

--- Financial-DB/fundamental.py
+++ Financial-DB/fundamental.py
@@ -2,8 +2,9 @@
 
 import requests
 import pandas as pd
+from db_connection import engine
 
-filename = r"Financial-DB\api_key.txt"
+filename = r"api_key.txt"
 
 with open(filename,'r') as file:
     api_key = file.readline()
@@ -15,6 +16,6 @@ exchange_list = requests.get(url).json()
 url = f'https://eodhd.com/api/exchange-symbol-list/{"US"}?api_token={api_key}&fmt=json'
 ticker_list = requests.get(url).json()
 
-pd.DataFrame.from_dict(exchange_list)
-pd.DataFrame.from_dict(ticker_list)
+pd.DataFrame.from_dict(exchange_list).to_sql(name="stg_exchange",con=engine,if_exists="replace")
+#pd.DataFrame.from_dict(ticker_list).to_sql(name="stg_")
 # %%

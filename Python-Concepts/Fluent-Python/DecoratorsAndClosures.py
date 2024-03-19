2929584bb06bc122e61d169b243dc68d5a8141c0
TheResearchLab <aarontellis1996@outlook.com>
2024-03-19 06:53:17 -0400

    reading: functools cache and lru_cache

--- Python-Concepts/Fluent-Python/DecoratorsAndClosures.py
+++ Python-Concepts/Fluent-Python/DecoratorsAndClosures.py
@@ -92,6 +92,103 @@ def make_averager():
         return total/count
     return averager
 
+#%%
+
+import time 
+
+def clock(func):
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapsed = time.perf_counter() - t0 
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print(f'[{elapsed:0.8f}s] {name}({arg_str}) -> {result!r}')
+        return result 
+    return clocked 
+
+@clock
+def snooze(seconds):
+    time.sleep(seconds)
+
+@clock
+def factorial(n):
+    return 1 if n < 2 else n*factorial(n-1)
+
+print('*'*40, 'Calling snooze(.123)')
+snooze(.123)
+print('*'*40,'Calling factorial(6)')
+factorial(6) # recursive function returning call multiple times as expected
+
+
+import functools 
+
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args,**kwargs):
+        t0 = time.perf_counter()
+        result = func(*args,**kwargs)
+        elapsed = time.perf_counter() - t0 
+        name = func.__name__
+        arg_lst = [repr(arg) for arg in args]
+        arg_lst.extend(f'{k}={v!r}' for k,v in kwargs.items()) # what is exclamation point r?
+        arg_str = ', '.join(arg_lst)
+        print(f'[{elapsed:0.8f}s] {name}({arg_str}) -> {result!r}')
+        return result 
+    return clocked 
+
+@clock
+def snooze(seconds):
+    time.sleep(seconds)
+
+@clock
+def factorial(n):
+    return 1 if n < 2 else n*factorial(n-1)
+
+print('*'*40, 'Calling snooze(.123)')
+snooze(.123)
+print('*'*40,'Calling factorial(6)')
+factorial(6) # recursive function returning call multiple times as expected
+
+# %%
+
+import functools
+
+## Memoization and cache decorator
+
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args,**kwargs):
+        t0 = time.perf_counter()
+        result = func(*args,**kwargs)
+        elapsed = time.perf_counter() - t0 
+        name = func.__name__
+        arg_lst = [repr(arg) for arg in args]
+        arg_lst.extend(f'{k}={v!r}' for k,v in kwargs.items()) # what is exclamation point r?
+        arg_str = ', '.join(arg_lst)
+        print(f'[{elapsed:0.8f}s] {name}({arg_str}) -> {result!r}')
+        return result 
+    return clocked 
+
+# @clock 
+# def fibonacci(n):
+#     if n < 2:
+#         return n
+#     return fibonacci(n-2) + fibonacci(n-1)
+
+# fibonacci(6)
+
+@functools.cache
+@clock 
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-2) + fibonacci(n-1)
+
+fibonacci(6)
 
 
 # %%
+
+# LRU_CACHE
+

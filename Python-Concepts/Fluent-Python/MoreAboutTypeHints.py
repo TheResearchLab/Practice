25a022ecd12d38e6d78919e8092acdc069be05e9
TheResearchLab <aarontellis1996@outlook.com>
2024-07-10 07:59:33 -0400

    reading CH-15 nominal type checking failures and type casting (pg. 537)

--- Python-Concepts/Fluent-Python/MoreAboutTypeHints.py
+++ Python-Concepts/Fluent-Python/MoreAboutTypeHints.py
@@ -23,19 +23,19 @@ class BookDict(TypedDict):
 # TypeDict Enables Mypy to catch errors
 from typing import TYPE_CHECKING
 
-def demo() -> None:
-    book = BookDict(
-        isbn='0134757599',
-        title='Refactoring, 2e',
-        authors=['Martin Fowler', 'Kent Beck'],
-        pagecount=478
-    )
-    authors = book['authors']
-    if TYPE_CHECKING:
-        reveal_type(authors)
-    authors = 'Bob' # this should be a lsit
-    book['weight'] = 4.2 # can't assign key that isn't part of the definition
-    del book['title'] # cannot delete a key that is part of the definition
+# def demo() -> None:
+#     book = BookDict(
+#         isbn='0134757599',
+#         title='Refactoring, 2e',
+#         authors=['Martin Fowler', 'Kent Beck'],
+#         pagecount=478
+#     )
+#     authors = book['authors']
+#     if TYPE_CHECKING:
+#         reveal_type(authors)
+#     authors = 'Bob' # this should be a lsit
+#     book['weight'] = 4.2 # can't assign key that isn't part of the definition
+#     del book['title'] # cannot delete a key that is part of the definition
 
 import json
 
@@ -49,8 +49,64 @@ def from_json(data:str) -> BookDict:
     whatever: BookDict = json.loads(data)
     return whatever
 
+AUTHOR_ELEMENT = '<AUTHOR>{}</AUTHOR>'
 
-if __name__ == '__main__':
-    demo()
+def to_xml(book: BookDict) -> str:
+    elements: list[str] = []
+    for key, value in book.items():
+        if isinstance(value,list):
+            elements.extend(
+                AUTHOR_ELEMENT.format(n) for n in value)
+        else:
+            tag = key.upper()
+            elements.append(f'<{tag}>{value}</{tag}>')
+    xml = '\n\t'.join(elements)
+    return f'<BOOK>\n\t{xml}\n</BOOK>'
+
+# example 2 - static type checking is unable to prevent error with inherently dynamic code ie Any
+def demo() -> None:
+    NOT_BOOK_JSON = """
+        {"title": "Andromed Strain",
+         "flavor": "pistachio",
+         "authors": true
+        }            
+        
+    """
+
+    not_book = from_json(NOT_BOOK_JSON)
+    if TYPE_CHECKING:
+        reveal_type(not_book)
+        reveal_type(not_book['authors'])
+
+    print(not_book)
+    print(not_book['flavor'])
+
+    xml = to_xml(not_book)
+    print(xml)
+
+
+# if __name__ == '__main__':
+#     demo()
+
+
+# Typing.cast to overcome the failures of nominal type checks
+
+def cast(typ, val):
+    """ Cast a value to a type.
+    This returns the value unchanged. To the type checker this
+    signals that the return value has the designated type, but
+    at runtime we intentionally don't check anything (we want
+    this to be as fast as possible)."""
+    return val 
+
+
+from typing import cast
+
+def find_first_str(a: list[object]) -> str:
+    index = next(i for i, x in enumerate(a) if isinstance(x, str))
+    # We only get here if there's at least on string
+    return cast(str, a[index])
+
+
 
 # %%

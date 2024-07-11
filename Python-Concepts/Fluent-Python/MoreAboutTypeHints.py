b59b274dc412a44a89be700a16abb588f4d50583
TheResearchLab <aarontellis1996@outlook.com>
2024-07-11 06:57:01 -0400

    reading: CH-15 typing annotation problems and generic types (pg. 544)

--- Python-Concepts/Fluent-Python/MoreAboutTypeHints.py
+++ Python-Concepts/Fluent-Python/MoreAboutTypeHints.py
@@ -110,3 +110,65 @@ def find_first_str(a: list[object]) -> str:
 
 
 # %%
+
+import abc
+import random
+
+from collections.abc import Iterable
+from typing import TypeVar, Generic 
+
+
+
+class Tombola(abc.ABC):
+    
+    @abc.abstractmethod
+    def load(self,iterable):
+        """ Add items from an iterable"""
+
+    @abc.abstractmethod
+    def pick(self):
+        """Remove item at random, return it.
+        
+        This method should raise 'LookupError' when the instance is empty
+        """
+    
+    def loaded(self):
+        """Return 'True' if there's at leadt 1 item, 'False' otherwise. """
+        return bool(self.inspect())
+    
+    def inspect(self):
+        """Return a sorted tuple with the items currently inside."""
+        items = []
+        while True:
+            try:
+                items.append(self.pick())
+            except LookupError:
+                break
+            self.load(items)
+            return tuple(items)
+        
+
+T = TypeVar('T')
+
+class LottoBlower(Tombola, Generic[T]):
+
+    def __init__(self,items: Iterable[T]) -> None:
+        self._balls = list[T](items)
+
+    def load(self, items: Iterable[T]) -> None:
+        self._balls.extend(items)
+
+    def pick(self) -> T:
+        try:
+            position = random.randrange(len(self._balls))
+        except ValueError:
+            raise LookupError('pick from empty LottoBlower')
+        return self._balls.pop(position)
+
+    def loaded(self) -> bool:
+        return bool(self._balls)
+    
+    def inspect(self) -> tuple[T,...]:
+        return tuple(self._balls)
+    
+        

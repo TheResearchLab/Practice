cbcfc897cfd47f79d9a05ecd40b278ee48491dae
TheResearchLab <aarontellis1996@outlook.com>
2024-07-18 07:42:35 -0400

    reading: CH-15 wrapping up invariant, covariant, contravariants, and generic static protocol (pg. 553)

--- Python-Concepts/Fluent-Python/MoreAboutTypeHints.py
+++ Python-Concepts/Fluent-Python/MoreAboutTypeHints.py
@@ -148,7 +148,7 @@ class Tombola(abc.ABC):
             return tuple(items)
         
 
-#T = TypeVar('T')
+T = TypeVar('T')
 
 class LottoBlower(Tombola, Generic[T]):
 
@@ -259,3 +259,42 @@ deploy(bio_can)
 # This does not work
 compost_can: TrashCan[Compostable] = TrashCan()
 deploy(compost_can)
+
+
+
+# Generic Static Protocol 
+
+import math 
+from typing import NamedTuple, SupportsAbs 
+
+class Vector2d(NamedTuple):
+    x: float 
+    y: float 
+
+    def __abs__(self) -> float:
+        return math.hypot(self.x,self.y)
+    
+
+def is_unit(v: SupportsAbs[float]) -> bool:
+    """'True' if the magnitude of 'v' is close to 1 """
+    return math.isclose(abs(v),1.0)
+
+assert issubclass(Vector2d,SupportsAbs)
+
+
+v0 = Vector2d(0,1)
+sqrt2 = math.sqrt(2)
+v1 = Vector2d(sqrt2/2, sqrt2/2)
+v2 = Vector2d(1,1)
+v3 = complex(.5, math.sqrt(3) / 2)
+v4 = 1 
+
+
+assert is_unit(v0)
+assert is_unit(v1)
+assert not is_unit(v2)
+assert is_unit(v3)
+assert is_unit(v4)
+
+print('OK')
+# %%

f1a8add8f57401a2b5fb7e55998f0d9364f5c34a
TheResearchLab <aarontellis1996@outlook.com>
2024-07-27 20:30:32 -0400

    reading: CH-16 + equality caveat  & infix and unary operators with vector class (pg. 572)

--- Python-Concepts/Fluent-Python/OperatorOverloading.py
+++ Python-Concepts/Fluent-Python/OperatorOverloading.py
@@ -7,4 +7,34 @@ periods = 12
 
 interest = principal * ((1 + rate) ** periods - 1)
 interest
+
+# When x != +x 
+# Change in context 
+import decimal 
+ctx = decimal.getcontext()
+ctx.prec = 40 
+one_third = decimal.Decimal('1') / decimal.Decimal('3')
+display(one_third) 
+
+one_third == +one_third
+ctx.prec = 28 
+print(one_third == +one_third) 
+display(+one_third)
+one_third
+
+# counter example
+from collections import Counter
+
+ct = Counter('abracadabra')
+ct
+ct['b'] = -3 
+ct['c'] = 0 
+display(ct)
++ct # removes the non positive instances
+
+
+
+
+
+
 # %%

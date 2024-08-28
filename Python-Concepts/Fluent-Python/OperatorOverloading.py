b9299e32116cec89c4548dd7e682fe44b3965f65
Aaron Tellis <aarontellis1996@outlook.com>
2024-08-28 07:33:42 -0400

    reading: CH-16 finish @ as an infix operator and start __eq__ method forward and reverse behavior (pg. 580)

--- Python-Concepts/Fluent-Python/OperatorOverloading.py
+++ Python-Concepts/Fluent-Python/OperatorOverloading.py
@@ -76,7 +76,9 @@ class Vector:
     
 
     def __eq__(self,other):
-        return len(self) == len(other) and all(a==b for a,b in zip(self,other))
+        if isinstance(other,Vector):
+            return len(self) == len(other) and all(a==b for a,b in zip(self,other))
+        return NotImplemented
 
     
     def __hash__(self):
@@ -180,7 +182,67 @@ class Vector:
         memv = memoryview(octets[1:]).cast(typecode)
         return cls(memv)
 
+class Vector2d:
+    typecode = 'd'
+    __match_args__ = ('x','y')
 
+    def __init__(self,x,y):
+        self.__x = float(x) 
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x 
+    
+    @property
+    def y(self):
+        return self.__y
+    
+    def __iter__(self):
+        return (i for i in (self.x,self.y))
+
+    def __hash__(self):
+        return hash((self.x,self.y))
+    
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r},{!r})'.format(class_name, *self)
+    
+    def __str__(self):
+        return str(tuple(self))
+    
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) +
+                bytes(array(self.typecode,self)))
+    
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+    
+    def __abs__(self):
+        return math.hypot(self.x,self.y)
+    
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __format__(self,fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self),self.angle())
+            outer_fmt = '<{},{}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c,fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+    
+    def angle(self):
+        return math.atan2(self.x,self.y)
+    
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return memv    
 
 v1 = Vector([3,4,5])
 
@@ -191,6 +253,17 @@ va = Vector([1,2,3])
 vz = Vector([5,6,7])
 print(va @ vz == 38.0) 
 
+# __eq__ method
+va = Vector([1.0,2.0,3.0])
+vb = Vector(range(1,4))
+va == vb 
+
+vc = Vector([1,2])
+v2d = Vector2d(1,2)
+vc == v2d 
+
+t3 = (1,2,3)
+va == t3
 
 
 # %%

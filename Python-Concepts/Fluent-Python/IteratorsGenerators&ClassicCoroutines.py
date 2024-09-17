9baf58124fd6ccb3be509e46f733420f68bde3bb
Aaron Tellis <aarontellis1996@outlook.com>
2024-09-17 19:22:36 -0400

    reading: CH-17 Implement Sentence class as iterable and create SentenceIterator class (pg. 605)

--- Python-Concepts/Fluent-Python/IteratorsGenerators&ClassicCoroutines.py
+++ Python-Concepts/Fluent-Python/IteratorsGenerators&ClassicCoroutines.py
@@ -64,3 +64,40 @@ next(it)
 next(it)
 #next(it) # stop iteration
 list(iter(s3))
+
+
+from collections import abc
+
+RE_WORD = re.compile('\w+')
+class Sentence:
+    
+    def __init__(self,text):
+        self.text = text 
+        self.words = RE_WORD.findall(text)
+    
+    def __repr__(self):
+        return f'Sentence({reprlib.repr(self.text)})'
+    
+    def __iter__(self):
+        return SentenceIterator(self.words)
+
+
+class SentenceIterator:
+
+    def __init__(self,words):
+        self.words = words 
+        self.index = 0
+    
+    def __next__(self):
+        try:
+            word = self.words[self.index]
+        except IndexError:
+            raise StopIteration()
+        self.index += 1
+        return word 
+    
+    def __iter__(self):
+        return self
+
+
+issubclass(SentenceIterator,abc.Iterator) # passes check  because SI implements __iter__ and __next__

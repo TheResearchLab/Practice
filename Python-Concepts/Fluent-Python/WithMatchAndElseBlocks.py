9a3f61df1918249e7fab1f9053fe2725ebad47b0
Aaron Tellis <aarontellis1996@outlook.com>
2024-10-14 07:44:43 -0400

    feat: CH-18 context manager decorator (pg. 669)

--- Python-Concepts/Fluent-Python/WithMatchAndElseBlocks.py
+++ Python-Concepts/Fluent-Python/WithMatchAndElseBlocks.py
@@ -25,3 +25,34 @@ with LookingGlass() as what:
 
 print(what) 
 print('Back to normal.')
+
+import contextlib 
+
+@contextlib.contextmanager # A with statement, a generator, and function decorator
+def looking_glass():
+    original_write = sys.stdout.write 
+
+    def reverse_write(text):
+        original_write(text[::-1])
+    
+    sys.stdout.write = reverse_write 
+    msg = ''
+    try:
+        yield 'JABBERWOCKY'
+    except ZeroDivisionError:
+        msg = 'Please DO NOT divide by zero!' 
+    finally: # logic that will always execute, so can close sessions in code
+        sys.stdout.write = original_write
+        if msg:
+            print(msg) 
+
+
+with looking_glass() as what:
+    print('Alice, Kitty, and Snowdrop')
+    print(what)
+
+print('back to normal')
+
+
+
+

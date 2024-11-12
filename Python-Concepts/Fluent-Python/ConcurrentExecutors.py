1475eacd295a1db5cf054af8144521af2a681ec5
Aaron Tellis <aarontellis1996@outlook.com>
2024-11-12 07:53:45 -0500

    reading: CH-20 exectuor map for sequential return results and sequential country code retrieval (pg. 769)

--- Python-Concepts/Fluent-Python/ConcurrentExecutors.py
+++ Python-Concepts/Fluent-Python/ConcurrentExecutors.py
@@ -134,7 +134,7 @@ def main() -> None:
 # if __name__ ==  '__main__':
 #     main()
 
-
+""" Executor Map """ # Get data back in the same order submitted
 from time import sleep, strftime 
 
 def display(*args):
@@ -162,5 +162,82 @@ if __name__ == '__main__':
     main()
 
 
+from collections import Counter 
+from http import HTTPStatus 
+from enum import Enum 
+
+
+import httpx 
+import tqdm # type: ignore
+
+DEFAULT_CONCUR_REQ = 1 
+MAX_CONCUR_REQ = 1
+
+
+DownloadStatus = Enum('DownloadStatus','OK NOT_FOUND ERROR')
+
+def save_flag(img:bytes, filename:str) -> None:
+    (DEST_DIR / filename).write_bytes(img)
+
+
+
+def main(downloader: Callable[[list[str]],int]) -> None:
+    DEST_DIR.mkdir(exist_ok=True)
+    t0 = time.perf_counter()
+    count = downloader(POP2_CC)
+    elapsed = time.perf_counter() - t0
+    print(f'\n{count} downloads in {elapsed:.2f}s')
+
+def get_flag(base_url: str, cc: str) -> bytes:
+    url = f'{base_url}/{cc}/{cc}.gif'.lower()
+    resp = httpx.get(url, timeout=3.1,follow_redirects=True)
+    resp.raise_for_status()
+    return resp.content 
+
+def download_one(cc:str, base_url: str, verbose: bool = False) -> DownloadStatus
+    try:
+        image = get_flag(base_url,cc)
+    except httpx.HTTPStatusError as exc:
+        res = exc.response 
+        if res.status_code == HTTPStatus.NOT_FOUND:
+            status = DownloadStatus.NOT_FOUND
+            msg = f'not found: {res.url}'
+        else:
+            raise 
+    else:
+        save_flag(image,f'{cc}.gif')
+        status = DownloadStatus.OK 
+        msg = 'OK'
+    
+    if verbose:
+        print(cc,msg)
+    
+    return status 
+
+def download_many(cc_list: list[str],
+                  base_url:str,
+                  verbose:bool,
+                  _unused_concur_req:int) -> Counter[DownloadStatus]:
+    counter: Counter[DownloadStatus] = Counter()
+    cc_iter = sorted(cc_list)
+    if not verbose:
+        cc_iter = tqdm.tqdm(cc_iter)
+    for cc in cc_iter:
+        try:
+            status = download_one(cc,base_url,verbose)
+        except httpx.HTTPStatusError as exc:
+            error_msg = f'{exc} {type(exc)}'.strip()
+        except KeyboardInterrupt:
+            break 
+        else:
+            error_msg = ''
+
+        if error_msg:
+            status = DownloadStatus.ERROR
+        counter[status] +=1
+        if verbose and error_msg:
+            print(f'{cc} error: {error_msg}')
+
+    return counter 
 
 

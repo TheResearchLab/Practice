f9379b34dd1e012bac1ae18830c78bc955e129e8
Aaron Tellis <aarontellis1996@outlook.com>
2024-11-19 08:03:51 -0500

    reading: CH-21 begin async country flag download with progress bar example (pg. 790)

--- Python-Concepts/Fluent-Python/AsyncProgramming.py
+++ Python-Concepts/Fluent-Python/AsyncProgramming.py
@@ -1,7 +1,7 @@
-import asyncio
-import socket 
+# import asyncio
+# import socket 
 
-from keyword import kwlist 
+# from keyword import kwlist 
 
 MAX_KEYWORD_LEN = 4
 
@@ -25,10 +25,10 @@ async def main() -> None:
 # if __name__ == '__main__':
 #     asyncio.run(main())
 
-import asyncio 
-from httpx import AsyncClient
+# import asyncio 
+# from httpx import AsyncClient
 
-from flags import BASE_URL, save_flag, main 
+# from flags import BASE_URL, save_flag, main 
 
 async def download_one(client: AsyncClient, cc: str) -> bytes:
     image = await get_flag(client,cc)
@@ -52,6 +52,50 @@ async def supervisor(cc_list: list[str]) -> int:
     
     return len(res)
 
-if __name__ == '__main__':
-    main(download_many)
+# if __name__ == '__main__':
+#     main(download_many)
 
+""" Progress Bar - Async get jobs 'as completed' """
+import asyncio
+from collections import Counter 
+from http import HTTPStatus 
+from pathlib import Path 
+
+import httpx 
+import tqdm #type: ignore
+
+from flags2_common import main, DownloadStatus, save_flag 
+
+DEFAULT_CONCUR_REQ = 5
+MAX_CONCUR_REQ = 1000 
+
+async def get_flag(client: httpx.AsyncClient,
+                   base_url: str,
+                   cc: str) -> bytes:
+    url = f'{base_url}/{cc}/{cc}.gif'.lower()
+    resp = await client.get(url,timeout=3.1,follow_redirects=True)
+    resp.raise_for_status()
+    return resp.content 
+
+async def download_one(client: httpx.AsyncClient,
+                       cc: str,
+                       base_url: str,
+                       semaphore: asyncio.Semaphore,
+                       verbose: bool) -> DownloadStatus:
+    try:
+        async with semaphore:
+            image = await get_flag(client, base_url, cc)
+    except httpx.HTTPStatusError as exc:
+        res = exc.response 
+        if res.status_code == HTTPStatus.NOT_FOUND:
+            status = DownloadStatus.NOT_FOUND 
+            msg = f'not found: {res.url}'
+        else:
+            raise
+    else:
+        await asyncio.to_thread(save_flag, image, f'{cc}.gif')
+        status = DownloadStatus.OK
+        msg = 'OK'
+    if verbose and msg:
+        print(cc,msg)
+    return status    

e5d0c4b9eee2bd894c39ef85bd9e1e31a94d7627
Aaron Tellis <aarontellis1996@outlook.com>
2024-11-19 19:53:45 -0500

    reading: CH-21 complete the flag2 example for async with as_completed (pg.794)

--- Python-Concepts/Fluent-Python/AsyncProgramming.py
+++ Python-Concepts/Fluent-Python/AsyncProgramming.py
@@ -30,27 +30,27 @@ async def main() -> None:
 
 # from flags import BASE_URL, save_flag, main 
 
-async def download_one(client: AsyncClient, cc: str) -> bytes:
-    image = await get_flag(client,cc)
-    save_flag(image,f'{cc}.gif')
-    print(cc,end=' ',flush=True)
-    return cc 
+# async def download_one(client: AsyncClient, cc: str) -> bytes:
+#     image = await get_flag(client,cc)
+#     save_flag(image,f'{cc}.gif')
+#     print(cc,end=' ',flush=True)
+#     return cc 
 
-async def get_flag(client: AsyncClient, cc: str) -> bytes:
-    url = f'{BASE_URL}/{cc}/{cc}.gif'.lower()
-    resp = await client.get(url, timeout=6.1,follow_redirects=True)
-    return resp.read()
+# async def get_flag(client: AsyncClient, cc: str) -> bytes:
+#     url = f'{BASE_URL}/{cc}/{cc}.gif'.lower()
+#     resp = await client.get(url, timeout=6.1,follow_redirects=True)
+#     return resp.read()
 
 
-def download_many(cc_list: list[str]) -> int:
-    return asyncio.run(supervisor(cc_list))
+# def download_many(cc_list: list[str]) -> int:
+#     return asyncio.run(supervisor(cc_list))
 
-async def supervisor(cc_list: list[str]) -> int:
-    async with AsyncClient() as client:
-        to_do = [download_one(client,cc) for cc in sorted(cc_list)]
-        res = await asyncio.gather(*to_do)
+# async def supervisor(cc_list: list[str]) -> int:
+#     async with AsyncClient() as client:
+#         to_do = [download_one(client,cc) for cc in sorted(cc_list)]
+#         res = await asyncio.gather(*to_do)
     
-    return len(res)
+#     return len(res)
 
 # if __name__ == '__main__':
 #     main(download_many)
@@ -66,7 +66,7 @@ import tqdm #type: ignore
 
 from flags2_common import main, DownloadStatus, save_flag 
 
-DEFAULT_CONCUR_REQ = 5
+DEFAULT_CONCUR_REQ = 1
 MAX_CONCUR_REQ = 1000 
 
 async def get_flag(client: httpx.AsyncClient,
@@ -99,3 +99,47 @@ async def download_one(client: httpx.AsyncClient,
     if verbose and msg:
         print(cc,msg)
     return status    
+
+async def supervisor(cc_list: list[str],
+                     base_url:str,
+                     verbose:bool,
+                     concur_req:int) -> Counter[DownloadStatus]:
+    counter: Counter[DownloadStatus] = Counter()
+    semaphore = asyncio.Semaphore(concur_req)
+    async with httpx.AsyncClient() as client:
+        to_do = [download_one(client,cc,base_url,semaphore,verbose) for cc in sorted(cc_list)]
+        to_do_iter = asyncio.as_completed(to_do)
+        if not verbose:
+            to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
+        error: httpx.HTTPError | None = None 
+        for coro in to_do_iter:
+            try:
+                status = await coro
+            except httpx.RequestError as exc:
+                error_msg = f'{exc} {type(exc)}'.strip()
+                error = exc 
+            except KeyboardInterrupt:
+                break 
+
+            if error:
+                status = DownloadStatus.ERROR
+                if verbose:
+                    url = str(error.request.url)
+                    cc = Path(url).stem.upper()
+                    print(f'{cc} error: {error_msg}')
+            counter[status] += 1
+
+    return counter 
+
+def download_many(cc_list: list[str],
+                  base_url: str,
+                  verbose: bool,
+                  concur_req: int) -> Counter[DownloadStatus]:
+    coro = supervisor(cc_list, base_url, verbose, concur_req)
+    counts = asyncio.run(coro)
+
+    return counts 
+
+if __name__ == '__main__':
+    main(download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ)
+

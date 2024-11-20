869fddac91acae6c72472ae2acd77b46b9de41cc
Aaron Tellis <aarontellis1996@outlook.com>
2024-11-19 20:09:20 -0500

    reading: CH-21 finish flags examples with multiple requests in a async function (pg.797)

--- Python-Concepts/Fluent-Python/AsyncProgramming.py
+++ Python-Concepts/Fluent-Python/AsyncProgramming.py
@@ -143,3 +143,40 @@ def download_many(cc_list: list[str],
 if __name__ == '__main__':
     main(download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ)
 
+""" Making Multiple Requests In One Task"""
+
+async def get_country(client: httpx.AsyncClient,
+                      base_url: str,
+                      cc: str) -> str:
+    url = f'{base_url}/{cc}/metadata.json'.lower()
+    resp = await client.get(url,timeout=3.1,follow_redirects=True)
+    resp.raise_for_status()
+    metadata = resp.json()
+    return metadata['country']
+
+async def download_one(client: httpx.AsyncClient,
+                       cc:str,
+                       base_url: str,
+                       semaphore: asyncio.Semaphore,
+                       verbose: bool) -> DownloadStatus:
+    try:
+        async with semaphore:
+            image =  await get_flag(client, base_url,cc)
+        async with semaphore:
+            country = await get_country(client, base_url,cc)
+    except httpx.HTTPStatusError as exc:
+        res = exc.response 
+        if res.status_code == HTTPStatus.NOT_FOUND:
+            status = DownloadStatus.NOT_FOUND 
+            msg = f'not found: {res.url}'
+        else:
+            raise 
+    else:
+        filename = country.replace(' ','_')
+        await asyncio.to_thread(save_flag,image,f'{filename}.gif')
+        status = DownloadStatus.OK
+        msg = 'OK'
+    if verbose and msg:
+        print(cc,msg)
+    return status 
+

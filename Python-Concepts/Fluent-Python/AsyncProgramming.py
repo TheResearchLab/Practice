f15c0001af1b5869f52edd6a480ae2c6ae68453b
Aaron Tellis <aarontellis1996@outlook.com>
2024-11-21 17:03:08 -0500

    reading: CH-21 add asyncio examples showing async comprehensions (pg. 821)

--- Python-Concepts/Fluent-Python/AsyncProgramming.py
+++ Python-Concepts/Fluent-Python/AsyncProgramming.py
@@ -56,127 +56,137 @@ async def main() -> None:
 #     main(download_many)
 
 """ Progress Bar - Async get jobs 'as completed' """
-import asyncio
-from collections import Counter 
-from http import HTTPStatus 
-from pathlib import Path 
+# import asyncio
+# from collections import Counter 
+# from http import HTTPStatus 
+# from pathlib import Path 
 
-import httpx 
-import tqdm #type: ignore
+# import httpx 
+# import tqdm #type: ignore
 
-from flags2_common import main, DownloadStatus, save_flag 
+# from flags2_common import main, DownloadStatus, save_flag 
 
-DEFAULT_CONCUR_REQ = 1
-MAX_CONCUR_REQ = 1000 
+# DEFAULT_CONCUR_REQ = 1
+# MAX_CONCUR_REQ = 1000 
 
-async def get_flag(client: httpx.AsyncClient,
-                   base_url: str,
-                   cc: str) -> bytes:
-    url = f'{base_url}/{cc}/{cc}.gif'.lower()
-    resp = await client.get(url,timeout=3.1,follow_redirects=True)
-    resp.raise_for_status()
-    return resp.content 
+# async def get_flag(client: httpx.AsyncClient,
+#                    base_url: str,
+#                    cc: str) -> bytes:
+#     url = f'{base_url}/{cc}/{cc}.gif'.lower()
+#     resp = await client.get(url,timeout=3.1,follow_redirects=True)
+#     resp.raise_for_status()
+#     return resp.content 
 
-async def download_one(client: httpx.AsyncClient,
-                       cc: str,
-                       base_url: str,
-                       semaphore: asyncio.Semaphore,
-                       verbose: bool) -> DownloadStatus:
-    try:
-        async with semaphore:
-            image = await get_flag(client, base_url, cc)
-    except httpx.HTTPStatusError as exc:
-        res = exc.response 
-        if res.status_code == HTTPStatus.NOT_FOUND:
-            status = DownloadStatus.NOT_FOUND 
-            msg = f'not found: {res.url}'
-        else:
-            raise
-    else:
-        await asyncio.to_thread(save_flag, image, f'{cc}.gif')
-        status = DownloadStatus.OK
-        msg = 'OK'
-    if verbose and msg:
-        print(cc,msg)
-    return status    
+# async def download_one(client: httpx.AsyncClient,
+#                        cc: str,
+#                        base_url: str,
+#                        semaphore: asyncio.Semaphore,
+#                        verbose: bool) -> DownloadStatus:
+#     try:
+#         async with semaphore:
+#             image = await get_flag(client, base_url, cc)
+#     except httpx.HTTPStatusError as exc:
+#         res = exc.response 
+#         if res.status_code == HTTPStatus.NOT_FOUND:
+#             status = DownloadStatus.NOT_FOUND 
+#             msg = f'not found: {res.url}'
+#         else:
+#             raise
+#     else:
+#         await asyncio.to_thread(save_flag, image, f'{cc}.gif')
+#         status = DownloadStatus.OK
+#         msg = 'OK'
+#     if verbose and msg:
+#         print(cc,msg)
+#     return status    
 
-async def supervisor(cc_list: list[str],
-                     base_url:str,
-                     verbose:bool,
-                     concur_req:int) -> Counter[DownloadStatus]:
-    counter: Counter[DownloadStatus] = Counter()
-    semaphore = asyncio.Semaphore(concur_req)
-    async with httpx.AsyncClient() as client:
-        to_do = [download_one(client,cc,base_url,semaphore,verbose) for cc in sorted(cc_list)]
-        to_do_iter = asyncio.as_completed(to_do)
-        if not verbose:
-            to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
-        error: httpx.HTTPError | None = None 
-        for coro in to_do_iter:
-            try:
-                status = await coro
-            except httpx.RequestError as exc:
-                error_msg = f'{exc} {type(exc)}'.strip()
-                error = exc 
-            except KeyboardInterrupt:
-                break 
+# async def supervisor(cc_list: list[str],
+#                      base_url:str,
+#                      verbose:bool,
+#                      concur_req:int) -> Counter[DownloadStatus]:
+#     counter: Counter[DownloadStatus] = Counter()
+#     semaphore = asyncio.Semaphore(concur_req)
+#     async with httpx.AsyncClient() as client:
+#         to_do = [download_one(client,cc,base_url,semaphore,verbose) for cc in sorted(cc_list)]
+#         to_do_iter = asyncio.as_completed(to_do)
+#         if not verbose:
+#             to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
+#         error: httpx.HTTPError | None = None 
+#         for coro in to_do_iter:
+#             try:
+#                 status = await coro
+#             except httpx.RequestError as exc:
+#                 error_msg = f'{exc} {type(exc)}'.strip()
+#                 error = exc 
+#             except KeyboardInterrupt:
+#                 break 
 
-            if error:
-                status = DownloadStatus.ERROR
-                if verbose:
-                    url = str(error.request.url)
-                    cc = Path(url).stem.upper()
-                    print(f'{cc} error: {error_msg}')
-            counter[status] += 1
+#             if error:
+#                 status = DownloadStatus.ERROR
+#                 if verbose:
+#                     url = str(error.request.url)
+#                     cc = Path(url).stem.upper()
+#                     print(f'{cc} error: {error_msg}')
+#             counter[status] += 1
 
-    return counter 
+#     return counter 
 
-def download_many(cc_list: list[str],
-                  base_url: str,
-                  verbose: bool,
-                  concur_req: int) -> Counter[DownloadStatus]:
-    coro = supervisor(cc_list, base_url, verbose, concur_req)
-    counts = asyncio.run(coro)
+# def download_many(cc_list: list[str],
+#                   base_url: str,
+#                   verbose: bool,
+#                   concur_req: int) -> Counter[DownloadStatus]:
+#     coro = supervisor(cc_list, base_url, verbose, concur_req)
+#     counts = asyncio.run(coro)
 
-    return counts 
+#     return counts 
 
-if __name__ == '__main__':
-    main(download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ)
+# if __name__ == '__main__':
+#     main(download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ)
 
 """ Making Multiple Requests In One Task"""
 
-async def get_country(client: httpx.AsyncClient,
-                      base_url: str,
-                      cc: str) -> str:
-    url = f'{base_url}/{cc}/metadata.json'.lower()
-    resp = await client.get(url,timeout=3.1,follow_redirects=True)
-    resp.raise_for_status()
-    metadata = resp.json()
-    return metadata['country']
+# async def get_country(client: httpx.AsyncClient,
+#                       base_url: str,
+#                       cc: str) -> str:
+#     url = f'{base_url}/{cc}/metadata.json'.lower()
+#     resp = await client.get(url,timeout=3.1,follow_redirects=True)
+#     resp.raise_for_status()
+#     metadata = resp.json()
+#     return metadata['country']
 
-async def download_one(client: httpx.AsyncClient,
-                       cc:str,
-                       base_url: str,
-                       semaphore: asyncio.Semaphore,
-                       verbose: bool) -> DownloadStatus:
-    try:
-        async with semaphore:
-            image =  await get_flag(client, base_url,cc)
-        async with semaphore:
-            country = await get_country(client, base_url,cc)
-    except httpx.HTTPStatusError as exc:
-        res = exc.response 
-        if res.status_code == HTTPStatus.NOT_FOUND:
-            status = DownloadStatus.NOT_FOUND 
-            msg = f'not found: {res.url}'
-        else:
-            raise 
-    else:
-        filename = country.replace(' ','_')
-        await asyncio.to_thread(save_flag,image,f'{filename}.gif')
-        status = DownloadStatus.OK
-        msg = 'OK'
-    if verbose and msg:
-        print(cc,msg)
-    return status 
+# async def download_one(client: httpx.AsyncClient,
+#                        cc:str,
+#                        base_url: str,
+#                        semaphore: asyncio.Semaphore,
+#                        verbose: bool) -> DownloadStatus:
+#     try:
+#         async with semaphore:
+#             image =  await get_flag(client, base_url,cc)
+#         async with semaphore:
+#             country = await get_country(client, base_url,cc)
+#     except httpx.HTTPStatusError as exc:
+#         res = exc.response 
+#         if res.status_code == HTTPStatus.NOT_FOUND:
+#             status = DownloadStatus.NOT_FOUND 
+#             msg = f'not found: {res.url}'
+#         else:
+#             raise 
+#     else:
+#         filename = country.replace(' ','_')
+#         await asyncio.to_thread(save_flag,image,f'{filename}.gif')
+#         status = DownloadStatus.OK
+#         msg = 'OK'
+#     if verbose and msg:
+#         print(cc,msg)
+#     return status
 
+""" Command Line Async Use - python -m asyncio to activate in terminal""" 
+# from domainlib import multi_probe
+# import asyncio
+
+# names = 'python.org rust-lang.org golang.org no-lang.invalid'.split()
+# gen_found = (name async for name, found in multi_probe(names) if found)
+# gen_found 
+
+# async for name in gen_found:
+#     print(name)

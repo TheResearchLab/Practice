5eb406597bfc80f5e411e202965567e95e932b12
Aaron Tellis <aarontellis1996@outlook.com>
2024-11-24 15:52:49 -0500

    reading: CH-22 Chapter Summary (pg.879)

--- Python-Concepts/Fluent-Python/DynamicAttributesAndProperties.py
+++ Python-Concepts/Fluent-Python/DynamicAttributesAndProperties.py
@@ -278,32 +278,39 @@
 #         return self.weight * self.price 
 
 """ Handling Attribute Deletion """
-class Demo:
-    pass 
+# class Demo:
+#     pass 
 
-d = Demo()
-d.color = 'green'
-print(d.color)
-del d.color
-print(d.color)
+# d = Demo()
+# d.color = 'green'
+# print(d.color)
+# del d.color
+# #print(d.color)
 
-class BlackKnight:
-    def __init__(self):
-        self.phrases = [
-        ('an arm', "'Tis but a scratch."),
-        ('another arm', "It's just a flesh wound."),
-        ('a leg', "I'm invincible!"),
-        ('another leg', "All right, we'll call it a draw.")
-        ]
-    @property
-    def member(self):
-        print('next member is:')
-        return self.phrases[0][0]
+# class BlackKnight:
+#     def __init__(self):
+#         self.phrases = [
+#         ('an arm', "'Tis but a scratch."),
+#         ('another arm', "It's just a flesh wound."),
+#         ('a leg', "I'm invincible!"),
+#         ('another leg', "All right, we'll call it a draw.")
+#         ]
+#     @property
+#     def member(self):
+#         print('next member is:')
+#         return self.phrases[0][0]
 
-    @member.deleter
-    def member(self):
-        member, text = self.phrases.pop(0)
-        print(f'BLACK KNIGHT (loses {member}) -- {text}')
+#     @member.deleter
+#     def member(self):
+#         member, text = self.phrases.pop(0)
+#         print(f'BLACK KNIGHT (loses {member}) -- {text}')
 
-    
+# knight = BlackKnight()
+# knight.member 
+# del knight.member 
+# del knight.member 
+# del knight.member 
+# del knight.member 
+
+
 

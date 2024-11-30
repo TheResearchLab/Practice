9c8b68636f0925a0cd0045bbcaa68e4037546131
Aaron Tellis <aarontellis1996@outlook.com>
2024-11-30 17:18:45 -0500

    reading: CH-24 Metaclasses, __new__, __prepare__ examples (pg. 942)

--- Python-Concepts/Fluent-Python/ClassMetaprogramming.py
+++ Python-Concepts/Fluent-Python/ClassMetaprogramming.py
@@ -72,71 +72,267 @@
 
 """ Checked Example for __init_subclass__ """
 
-from collections.abc import Callable 
-from typing import Any, NoReturn, get_type_hints 
+# from collections.abc import Callable 
+# from typing import Any, NoReturn, get_type_hints 
 
-class Field:
-    def __init__(self, name:str, constructor: Callable) -> None:
-        if not callable(constructor)  or constructor is type(None):
-            raise TypeError(f'{name!r} type hint must be callable')
-        self.name = name 
-        self.constructor = constructor
+# class Field:
+#     def __init__(self, name:str, constructor: Callable) -> None:
+#         if not callable(constructor)  or constructor is type(None):
+#             raise TypeError(f'{name!r} type hint must be callable')
+#         self.name = name 
+#         self.constructor = constructor
     
-    def __set__(self,instance:Any, value: Any) -> None:
-        if value is ...:
-            value = self.constructor()
-        else:
-            try:
-                value = self.constructor(value)
-            except (TypeError,ValueError) as e:
-                type_name = self.constructor.__name__
-                msg = f'{value!r} is not compatible with {self.name}:{type_name}'
-                raise TypeError(msg) from e 
-        instance.__dict__[self.name] = value 
+#     def __set__(self,instance:Any, value: Any) -> None:
+#         if value is ...:
+#             value = self.constructor()
+#         else:
+#             try:
+#                 value = self.constructor(value)
+#             except (TypeError,ValueError) as e:
+#                 type_name = self.constructor.__name__
+#                 msg = f'{value!r} is not compatible with {self.name}:{type_name}'
+#                 raise TypeError(msg) from e 
+#         instance.__dict__[self.name] = value 
 
-class Checked:
-    @classmethod
-    def _fields(cls) -> dict[str, type]:
-        return get_type_hints(cls)
+# class Checked:
+#     @classmethod
+#     def _fields(cls) -> dict[str, type]:
+#         return get_type_hints(cls)
     
-    def __init_subclass__(subclass) -> None:
-        super().__init_subclass__()
-        for name, constructor in subclass._fields().items():
-            setattr(subclass, name, Field(name,constructor))
+#     def __init_subclass__(subclass) -> None:
+#         super().__init_subclass__()
+#         for name, constructor in subclass._fields().items():
+#             setattr(subclass, name, Field(name,constructor))
     
-    def __init__(self,**kwargs: Any) -> None:
-        for name in self._fields():
-            value = kwargs.pop(name,...)
-            setattr(self, name, value)
-        if kwargs:
-            self.__flag_unknown_attrs(*kwargs)
+#     def __init__(self,**kwargs: Any) -> None:
+#         for name in self._fields():
+#             value = kwargs.pop(name,...)
+#             setattr(self, name, value)
+#         if kwargs:
+#             self.__flag_unknown_attrs(*kwargs)
 
-    def __flag_unknown_attrs(self, *names: str) -> NoReturn:
-        plural = 's' if len(names) > 1 else ''
-        extra = ', '.join(f'{name!r}' for name in names)
-        cls_name = repr(self.__class__.__name__)
-        raise AttributeError(f'{cls_name} object has no attribute{plural} {extra}')
+#     def __flag_unknown_attrs(self, *names: str) -> NoReturn:
+#         plural = 's' if len(names) > 1 else ''
+#         extra = ', '.join(f'{name!r}' for name in names)
+#         cls_name = repr(self.__class__.__name__)
+#         raise AttributeError(f'{cls_name} object has no attribute{plural} {extra}')
     
-    def _asdict(self)  -> dict[str,Any]:
-        return {
-            name: getattr(self,name)
-            for name, attr in self.__class__.__dict__.items()
-            if isinstance(attr,Field)
-        }
+#     def _asdict(self)  -> dict[str,Any]:
+#         return {
+#             name: getattr(self,name)
+#             for name, attr in self.__class__.__dict__.items()
+#             if isinstance(attr,Field)
+#         }
     
-    def __repr___(self) -> str:
-        kwargs = ', '.join(f'{key}={value}' for key, value in self._asdict().items())
-        return f'{self.__class__.__name__}({kwargs})'
+#     def __repr___(self) -> str:
+#         kwargs = ', '.join(f'{key}={value}' for key, value in self._asdict().items())
+#         return f'{self.__class__.__name__}({kwargs})'
     
 
-class Movie(Checked):
-    title: str 
-    year: int 
-    box_office: float 
+# class Movie(Checked):
+#     title: str 
+#     year: int 
+#     box_office: float 
 
-movie = Movie(title='The God Father',year=1972,box_office=137)
-print(movie.title)
-print(movie)
+# movie = Movie(title='The God Father',year=1972,box_office=137)
+# print(movie.title)
+# print(movie)
+
+""" Enhancing Classes with Class Decorator """
+# from collections.abc import Callable 
+# from typing import Any, NoReturn, get_type_hints 
+
+# class Field:
+#     def __init__(self, name:str, constructor: Callable) -> None:
+#         if not callable(constructor)  or constructor is type(None):
+#             raise TypeError(f'{name!r} type hint must be callable')
+#         self.name = name 
+#         self.constructor = constructor
+    
+#     def __set__(self,instance:Any, value: Any) -> None:
+#         if value is ...:
+#             value = self.constructor()
+#         else:
+#             try:
+#                 value = self.constructor(value)
+#             except (TypeError,ValueError) as e:
+#                 type_name = self.constructor.__name__
+#                 msg = f'{value!r} is not compatible with {self.name}:{type_name}'
+#                 raise TypeError(msg) from e 
+#         instance.__dict__[self.name] = value 
+
+# def _fields(cls:type) -> dict[str,type]:
+#     return get_type_hints(cls)
+
+# def __init__(self: Any, **kwargs:Any) -> None:
+#     for name in self._fields():
+#         value = kwargs.pop(name,...)
+#         setattr(self,name,value)
+#     if kwargs:
+#         self.__flag_unknown_attrs(*kwargs)
+
+# def __setattr__(self: Any, name: str, value: Any) -> None:
+#     if name in self._fields():
+#         cls = self.__class__ 
+#         descriptor = getattr(cls,name)
+#         descriptor.__set__(self,value)
+#     else:
+#         self.__flag_unknown_attrs(name)
+
+# def __flag_unknown_attrs(self: Any, *names:str) -> NoReturn:
+#     plural = 's' if len(names) > 1 else ''
+#     extra = ', '.join(f'{name!r}' for name in names)
+#     cls_name = repr(self.__class__.__name__)
+#     raise AttributeError(f'{cls_name} has no attribute{plural} {extra}')
 
 
+# def _asdict(self:Any) -> dict[str,Any]:
+#     return {
+#         name: getattr(self,name)
+#         for name, attr in self.__class__.__dict__.items()
+#         if isinstance(attr,Field)
+#     }
+
+# def __repr__(self: Any) -> str:
+#     kwargs = ', '.join(
+#         f'{key}={value}' for key, value in self._asdict().items()
+#     )
+#     return f'{self.__class__.__name__}({kwargs})'
+
+# def checked(cls:type) -> type:
+#     for name, constructor in _fields(cls).items():
+#         setattr(cls,name,Field(name,constructor))
+    
+#     cls._fields = classmethod(_fields) #type: ignore 
+
+#     instance_methods = (
+#         __init__,
+#         __repr__,
+#         __setattr__,
+#         _asdict,
+#         __flag_unknown_attrs,
+#     )
+#     for method in instance_methods:
+#         setattr(cls, method.__name__,method)
+    
+#     return cls
+
+
+# @checked 
+# class Movie:
+#     title: str 
+#     year: int
+#     box_office: float 
+
+# movie = Movie(title='The Godfather',year=1972,box_office=137)
+# print(movie.title)
+# print(movie)
+
+""" Evaluation Time Experiment """
+# import builderlib
+
+""" Eval Demo builderlib """
+# from builderlib import Builder, deco, Descriptor 
+# print('# evaldemo module start')
+
+# @deco 
+# class Klass(Builder):
+#     print('# Klass body')
+
+#     attr = Descriptor()
+
+#     def __init__(self):
+#         super().__init__()
+#         print(f'# Klass.__init__({self!r})')
+
+#     def __repr__(self):
+#         return '<Klass instance>'
+    
+# def main():
+#     obj = Klass()
+#     obj.method_a()
+#     obj.method_b()
+#     obj.attr = 999 
+
+# if __name__ == '__main__':
+#     main()
+
+# print('# evaldemo module end')
+
+""" MetaBunch metaclass example """
+# class MetaBunch(type):
+#     def __new__(meta_cls, cls_name, bases, cls_dict):
+#         defaults = {}
+
+#         def __init__(self,**kwargs):
+#             for name, default in defaults.items():
+#                 setattr(self,name,kwargs.pop(name,default))
+#             if kwargs:
+#                 extra = ', '.join(kwargs)
+#                 raise AttributeError(f'No slots left for: {extra!r}')
+            
+#         def __repr__(self):
+#             rep = ', '.join(f'{name}={value}'
+#                             for name, default in defaults.items()
+#                             if (value:= getattr(self, name)) != default)
+#             return f'{cls_name}({rep})'
+            
+#         new_dict = dict(__slots__=[],__init__=__init__,__repr__=__repr__)
+
+#         for name, value in cls_dict.items():
+#             if name.startswith('__') and name.endswith('__'):
+#                 if name in new_dict:
+#                     raise AttributeError(f"Can't set {name!r} in {cls_name!r}")
+#                 new_dict[name] = value 
+#             else:
+#                 new_dict['__slots__'].append(name)
+#                 defaults[name] = value 
+#         return super().__new__(meta_cls, cls_name, bases,new_dict)
+
+# class Bunch(metaclass=MetaBunch):
+#     pass 
+
+# class Point(Bunch):
+#     x = 0.0
+#     y = 0.0 
+#     color = 'gray'
+
+# print(Point(x=1.2,y=3,color='green'))
+# p = Point()
+# print(p)
+# print((p.x,p.y,p.color))
+
+# p = Point(x=21)
+# p.y=42
+# print(p)
+# p.flavor = 'banana'
+
+""" Metaclass Evaluation Time Experiment """
+from builderlib import Builder, deco, Descriptor 
+from metalib import MetaKlass 
+
+@deco 
+class Klass(Builder, metaclass=MetaKlass):
+    print('# Klass body')
+
+    attr = Descriptor()
+
+    def __init__(self):
+        super().__init__()
+        print(f'# Klass.__init__({self!r})')
+    
+    def __repr__(self):
+        return '<Klass instance>'
+    
+def main():
+    obj = Klass()
+    obj.method_a()
+    obj.method_b()
+    obj.method_c()
+    obj.attr = 999 
+
+if __name__ == '__main__':
+    main()
+
+print('# evaldemo_meta module end')
 
